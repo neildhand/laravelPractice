a1f80e63b45e4580fe1ff20a25c808750e792441
Neil Dhand <neil_dhand@baylor.edu>
2018-07-12 15:25:44 -0500

    able to create post with static category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\PostsCreateRequest;
+use App\Post;
+use App\Photo;
+use Auth;
 
 class AdminPostsController extends Controller
 {
@@ -16,6 +20,9 @@ class AdminPostsController extends Controller
     public function index()
     {
         //
+        $posts = Post::all();
+        
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -26,6 +33,7 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
+        return view('admin.posts.create');
     }
 
     /**
@@ -34,9 +42,26 @@ class AdminPostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostsCreateRequest $request)
     {
         //
+        //return $request->all();
+        $input = $request->all();
+        $user = Auth::user();
+
+        if($file = $request->file('photo_id')){
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+            
+            $photo = Photo::create(['file'=>$name]);
+            
+            $input['photo_id'] = $photo->id;
+        }
+        
+        $user->posts()->create($input);
+        return redirect('/admin/posts');
+
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
         }
         return false;
     }
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
