de7c5c644a331321a686e3ae5dff8fbdbf99005a
Neil Dhand <neil_dhand@baylor.edu>
2018-07-15 22:09:21 -0500

    added comment view, controller, model, migrations, and relationships

---
+++ app/CommentReply.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommentReply extends Model
+{
+    //
+
+    protected $fillable = [
+        'comment_id',
+        'author',
+        'email',
+        'body',
+        'is_active'
+    ];
+
+    public function comment(){
+        $this->belongsTo('App\Comment');
+    }
+}
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,6 +16,8 @@
 
     <link href="{{asset('css/libs.css')}}" rel="stylesheet">
 
+    @yield('styles')
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -145,6 +147,10 @@
                                 <a href="{{route('admin.posts.create')}}">Create Post</a>
                             </li>
 
+                            <li>
+                                    <a href="{{route('admin.comments.index')}}">All Comments</a>
+                                </li>
+
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
@@ -170,20 +176,16 @@
                         <a href="#"><i class="fa fa-wrench fa-fw"></i>Media<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="/media">All Media</a>
+                            <a href="{{route('admin.media.index')}}">All Media</a>
                             </li>
 
                             <li>
-                                <a href="">Upload Media</a>
+                            <a href="{{route('admin.media.create')}}">Upload Media</a>
                             </li>
 
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
-
-
-
-
 
 
 
@@ -350,7 +352,9 @@
 <script src="{{asset('js/libs.js')}}"></script>
 
 
-@yield('footer')
+
+
+@yield('scripts')
 
 
 
